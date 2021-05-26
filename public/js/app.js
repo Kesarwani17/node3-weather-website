13ed3d641da70ad4c3c531578db48288d81d143e
Gaurav Kesarwani <gauravkesarwani94@gmail.com>
2021-05-26 18:11:43 +0530

    added precipitation and commented things!

--- public/js/app.js
+++ public/js/app.js
@@ -20,7 +20,7 @@ weatherForm.addEventListener('submit', (e) => {
             } else {
                 messageOne.textContent = data.address + "'s"+' Weather!'
                 messageTwo.textContent = 'Weather at '+data.address+' is '+data.forecast.weather+'. It is '+data.forecast.temperature+'C and feels like '+data.forecast.feels_like+'C at '+data.location + '. The cloud cover is '+data.forecast.cloud_cover+'% and humidity is '+data.forecast.humidity+' .The chances of rain are '+data.forecast.precip+'%'                // console.log(data.location)
-                // console.log(data.forecast)
+                //console.log(data.forecast)
             }
         })
     })
