7f963b561b4520c2ce330eff89d50f67a2fe1af1
Gaurav Kesarwani <gauravkesarwani94@gmail.com>
2021-05-26 18:08:47 +0530

    added precipitation and commented things!

--- public/js/app.js
+++ public/js/app.js
@@ -3,6 +3,7 @@ const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
 const messageTwo = document.querySelector('#message-2')
 
+
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
     
@@ -18,8 +19,7 @@ weatherForm.addEventListener('submit', (e) => {
                 messageOne.textContent = data.error
             } else {
                 messageOne.textContent = data.address + "'s"+' Weather!'
-                messageTwo.textContent = 'Weather at '+data.address+' is '+data.forecast.weather+'. It is '+data.forecast.temperature+'C and feels like '+data.forecast.feels_like+'C at '+data.location + '. The cloud cover is '+data.forecast.cloud_cover+'% and humidity is '+data.forecast.humidity
-                // console.log(data.location)
+                messageTwo.textContent = 'Weather at '+data.address+' is '+data.forecast.weather+'. It is '+data.forecast.temperature+'C and feels like '+data.forecast.feels_like+'C at '+data.location + '. The cloud cover is '+data.forecast.cloud_cover+'% and humidity is '+data.forecast.humidity+' .The chances of rain are '+data.forecast.precip+'%'                // console.log(data.location)
                 // console.log(data.forecast)
             }
         })
